7980e4206f14a2339cfea7675e5ab4dd766d6a84
paperworker.hsg@gmail.com <paperworker.hsg@gmail.com>
2023-05-28 16:28:36 +0900

    first commit

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("#clock");
+
+function sayHello() {
+  console.log("hello");
+}
+
+function getClock() {
+  const date = new Date();
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hour}:${minutes}:${seconds}`;
+}
+getClock();
+setInterval(getClock, 1000);
